f9fd4d5adbcc89fac7a0270767b0fcfa4caeb28b
david <davidgking@163.com>
2020-02-02 14:13:09 +0800

    update for swagger spec

--- src/main/java/com/hase/huatuo/healthcheck/model/request/VpnReportRequest.java
+++ src/main/java/com/hase/huatuo/healthcheck/model/request/VpnReportRequest.java
@@ -1,6 +1,11 @@
 package com.hase.huatuo.healthcheck.model.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "RequestBody of VPN state report")
 public class VpnReportRequest {
+    @ApiModelProperty(value = "0", notes = "0 or null means summary from 2019-12-01, 1 means summary of 1 day before", dataType = "java.lang.Integer")
     private Integer day;
 
     public Integer getDay() {
