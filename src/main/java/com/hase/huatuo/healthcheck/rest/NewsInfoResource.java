1a0ae97453bbbb8b371422ba60c3697f3e92b4c3
hunter <1447037349@qq.com>
2020-02-04 18:05:11 +0800

    news show detail and update item status

--- src/main/java/com/hase/huatuo/healthcheck/rest/NewsInfoResource.java
+++ src/main/java/com/hase/huatuo/healthcheck/rest/NewsInfoResource.java
@@ -2,10 +2,12 @@ package com.hase.huatuo.healthcheck.rest;
 
 import com.hase.huatuo.healthcheck.dao.NewsStatusRepository;
 import com.hase.huatuo.healthcheck.dao.NewsInfoRepository;
+import com.hase.huatuo.healthcheck.error.ErrorMessage;
 import com.hase.huatuo.healthcheck.model.ItemStatus;
 import com.hase.huatuo.healthcheck.model.NewsInfo;
 import com.hase.huatuo.healthcheck.model.request.NewsDetailRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,13 +26,20 @@ public class NewsInfoResource {
         final String newsId = detail.getNewsId();
         final String staffId = detail.getStaffId();
        ItemStatus itemStatus  = newsStatusRep.getItemStatusBy(newsId,staffId);
-       if(itemStatus.getItemType() == "N"){
+       if("N".equals(itemStatus.getItemType())){
            itemStatus.setItemType("Y");
            newsStatusRep.saveAndFlush(itemStatus);
        }
-       Optional<NewsInfo> info = newsInfo.findById(newsId);
 
-       return ResponseEntity.ok(info.get().getContent());
+       Optional<NewsInfo> info = newsInfo.findById(newsId);
+       ResponseEntity response;
+        if("N".equals(info.get().getEnable())){
+           response = new ResponseEntity(new ErrorMessage("error", "item new readable")
+                   ,HttpStatus.BAD_REQUEST);
+        }else {
+            response = ResponseEntity.ok(info.get().getContent());
+        }
+       return response;
     }
 
 
