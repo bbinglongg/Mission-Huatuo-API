b4051a55aa381a3107aadc6271c91b1150ade85a
j20lin <j20lin@126.com>
2020-02-06 01:44:44 +0800

    merge code

--- src/main/java/com/hase/huatuo/healthcheck/rest/HuatuoResource.java
+++ src/main/java/com/hase/huatuo/healthcheck/rest/HuatuoResource.java
@@ -1,167 +1,59 @@
 package com.hase.huatuo.healthcheck.rest;
 
 import java.text.ParseException;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hase.huatuo.healthcheck.helper.ErrorHandleHelper;
-import com.hase.huatuo.healthcheck.model.SMSInfo;
-import com.hase.huatuo.healthcheck.model.request.HealthPostBody;
-import com.hase.huatuo.healthcheck.model.request.NewsInfoListRequestBody;
-import com.hase.huatuo.healthcheck.model.request.NewsNotReadRequest;
-import com.hase.huatuo.healthcheck.model.request.RegistrationPostBody;
-import com.hase.huatuo.healthcheck.model.request.StaffOfHacnNeedsPostBody;
-import com.hase.huatuo.healthcheck.model.request.VpnReportRequest;
-import com.hase.huatuo.healthcheck.model.request.VpnRequest;
-import com.hase.huatuo.healthcheck.model.request.WechatLoginRequest;
-import com.hase.huatuo.healthcheck.model.response.AreaReport;
-import com.hase.huatuo.healthcheck.model.response.AreaReportForHacn;
+import com.hase.huatuo.healthcheck.model.request.TripDetailReqBody;
+import com.hase.huatuo.healthcheck.model.request.TripHistoryReqBody;
+import com.hase.huatuo.healthcheck.model.request.TripSurveyPostBody;
 import com.hase.huatuo.healthcheck.model.response.CommonResponse;
-import com.hase.huatuo.healthcheck.model.response.DatadictGetResponse;
-import com.hase.huatuo.healthcheck.model.response.HealthPostResponse;
-import com.hase.huatuo.healthcheck.model.response.NewsInfoListResponse;
-import com.hase.huatuo.healthcheck.model.response.VpnReportResponse;
-import com.hase.huatuo.healthcheck.model.response.WechatLoginResponse;
-import com.hase.huatuo.healthcheck.service.HealthReportOfHacnService;
-import com.hase.huatuo.healthcheck.service.HealthReportService;
-import com.hase.huatuo.healthcheck.service.HuatuoHealthService;
-import com.hase.huatuo.healthcheck.service.HuatuoNewsService;
-import com.hase.huatuo.healthcheck.service.HuatuoRegistrationService;
-import com.hase.huatuo.healthcheck.service.HuatuoVPNService;
-import com.hase.huatuo.healthcheck.service.HuatuoWechatService;
-import com.hase.huatuo.healthcheck.service.NewsInfoListService;
-import com.hase.huatuo.healthcheck.service.StaffNeedsCollectionsOfHacnService;
+import com.hase.huatuo.healthcheck.service.HuatuoTripDetailService;
+import com.hase.huatuo.healthcheck.service.HuatuoTripHistoryService;
+import com.hase.huatuo.healthcheck.service.HuatuoTripSurveyService;
 
 import io.swagger.annotations.ApiOperation;
-import me.chanjar.weixin.common.error.WxErrorException;
 
 
 @RestController
-@RequestMapping({"/huatuo/api","/huatuo/api/v1","/api","/api/v1"})
-public class HuatuoResource {
+@RequestMapping({"/api/hacn/","/api/hacn/v1"})
+public class HuatuoHacnResource {
 
 
 	@Autowired
-    private HuatuoHealthService huatuoHealthService;
+    private HuatuoTripHistoryService huatuoTripHistoryService;
+	
+	@Autowired
+    private HuatuoTripDetailService huatuoTripDetailService;
+	
+	@Autowired
+    private HuatuoTripSurveyService huatuoTripSurveyService;
 
-    @Autowired
-    private HuatuoVPNService huatuoVPNService;
 
-    @Autowired
-    private HealthReportService healthReportService;
-
-    @Autowired
-    private HuatuoRegistrationService huatuoRegistrationService;
-
-    @Autowired
-    private HuatuoWechatService huatuoWechatService;
-
-    @Autowired
-    private StaffNeedsCollectionsOfHacnService needsCollectionsOfHacnService;
-
-    @Autowired
-    private HealthReportOfHacnService healthReportOfHacnService;
-    
-    @Autowired
-    private NewsInfoListService newsInfoListService;
-    
-    @Autowired
-    private HuatuoNewsService huatuoNewsService;
-
-    @PostMapping("/health")
-    public HealthPostResponse updateHealth(@RequestBody final HealthPostBody healthPostBody) {
-    	validHealthRequest(healthPostBody);
-    	
-    	return huatuoHealthService.setPersonHealth(healthPostBody);
+    @PostMapping("/trip/history")
+    @ApiOperation(value = "hacn-trip-history", notes = "hacn trip history", httpMethod = "POST")
+    public ResponseEntity getTripHistory(@Valid @RequestBody final TripHistoryReqBody tripHistoryBody) {
+    	return huatuoTripHistoryService.getTripHistory(tripHistoryBody);
     }
     
-    @GetMapping("/health")
-    public ResponseEntity<List<AreaReport>> requestHealth(@RequestHeader("X-IS-DUMMY") String isDummy) {
-    	return healthReportService.enquiry(isDummy);
-    }
-
-    @GetMapping("/hacn/health")
-    public ResponseEntity<AreaReportForHacn> requestHealthOfHacn() {
-        return healthReportOfHacnService.enquiry();
+    @PostMapping("/trip/detail")
+    @ApiOperation(value = "hacn-trip-detail", notes = "hacn trip detail", httpMethod = "POST")
+    public ResponseEntity getTripDetail(@Valid @RequestBody final TripDetailReqBody tripDetailReqBody) {
+    	return huatuoTripDetailService.getTripDetail(tripDetailReqBody);
     }
     
-    @PostMapping("/vpn")
-    @ApiOperation(value = "vpnState", notes = "Save or update staff VPN state", httpMethod = "POST")
-    public ResponseEntity submitVPNState(@RequestBody final VpnRequest vpnRequest) {
-        huatuoVPNService.submitVPNState(vpnRequest);
-    	return ResponseEntity.ok(null);
-    }
-
-    @PostMapping("/vpn/report")
-    @ApiOperation(value = "vpnReport", notes = "Index VPN status report", httpMethod = "POST")
-    public ResponseEntity<VpnReportResponse> loadVPNStateDashboard(@RequestBody final VpnReportRequest vpnReportRequest) throws ParseException {
-        return ResponseEntity.ok(new VpnReportResponse(huatuoVPNService.loadVPNStateDashboard(vpnReportRequest)));
+    @PostMapping("/add_tripSurvey")
+    @ApiOperation(value = "add_tripSurvey", notes = "add tripSurvey", httpMethod = "POST")
+    public ResponseEntity<CommonResponse> addTripSurvey(@RequestBody final TripSurveyPostBody tripSurveyPostBody) throws ParseException {
+//        huatuoTripSurveyService.validAddTripSurveyRequest(registerBody);
+        return huatuoTripSurveyService.addTripSurvey(tripSurveyPostBody);
     }
     
-    @GetMapping("/datadict")
-    @ApiOperation(value = "datadict", notes = "Get Datadict From API", httpMethod = "GET")
-    public ResponseEntity<DatadictGetResponse> getDatadict() throws ParseException {
-		return ResponseEntity.ok(new DatadictGetResponse());
-    }
-
-    @PostMapping("/sendSMSVerifyCode")
-    @ApiOperation(value = "sendSMSVerifyCode", notes = "send SMS verify code", httpMethod = "POST")
-    public ResponseEntity<CommonResponse> sendSMSVerifyCode(@RequestBody final SMSInfo smsInfo) throws ParseException {
-        return huatuoRegistrationService.sendVerifyCode(smsInfo);
-    }
-
-    @PostMapping("/register")
-    @ApiOperation(value = "register", notes = "register user", httpMethod = "POST")
-    public ResponseEntity<CommonResponse> registration(@RequestBody final RegistrationPostBody registerBody) throws ParseException {
-        huatuoRegistrationService.validRegisterRequest(registerBody);
-        return huatuoRegistrationService.register(registerBody);
-    }
-
-    private void validHealthRequest(HealthPostBody healthPostBody) {
-    	if(healthPostBody == null ) {
-    		ErrorHandleHelper.getInstance().throwBadRequestRestException("Bad Request", "request body is null", null);
-    	}
-    	
-    	if(healthPostBody.getStaffId() == null ||
-    			healthPostBody.getStaffId().length() < 5) {
-    		ErrorHandleHelper.getInstance().throwBadRequestRestException("Bad Request", "staff ID is error", null);
-    	}
-    	
-    	// other field checking
-    		
-    }
-
-    @PostMapping("/wechat-login")
-    @ApiOperation(value = "wechat-login", notes = "wechat login", httpMethod = "POST")
-    public ResponseEntity<WechatLoginResponse> login(@RequestBody final WechatLoginRequest wechatLoginRequest) throws WxErrorException {
-        return ResponseEntity.ok(huatuoWechatService.login(wechatLoginRequest.getAppId(), wechatLoginRequest.getCode()));
-    }
-
-    @PostMapping("/hacn/needs-collection")
-    @ApiOperation(value = "hacn-needs-collection", notes = "hacn staff needs collection", httpMethod = "POST")
-    public ResponseEntity needsCollectionOfHacnStaff(@Valid @RequestBody  StaffOfHacnNeedsPostBody staffOfHacnNeedsPostBody){
-        return needsCollectionsOfHacnService.saveStaffNeedsCollection(staffOfHacnNeedsPostBody);
-    }
-
-    @PostMapping("/news-info/lists")
-    public NewsInfoListResponse newsInfoList(@RequestBody final NewsInfoListRequestBody newsInfoListRequestBody) {
-    	
-    	return newsInfoListService.getNewsInfoList(newsInfoListRequestBody);
-    }
-
-    @PostMapping("/important-news")
-    @ApiOperation(value = "important-news", notes = "important news and unread number", httpMethod = "POST")
-    public ResponseEntity<CommonResponse> getImportantNewsList(@RequestBody final NewsNotReadRequest newsNotReadReq) throws WxErrorException {
-        return huatuoNewsService.getImportantNewsList(newsNotReadReq);
-    }
 }
