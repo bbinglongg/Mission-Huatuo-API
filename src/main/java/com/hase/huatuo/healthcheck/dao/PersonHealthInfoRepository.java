8e42f75902a0d3f4c7c713a1414943c000976bf9
duyantao <duyantao@sina.cn>
2020-02-08 21:47:28 +0800

    fix the build error

--- src/main/java/com/hase/huatuo/healthcheck/dao/PersonHealthInfoRepository.java
+++ src/main/java/com/hase/huatuo/healthcheck/dao/PersonHealthInfoRepository.java
@@ -4,8 +4,13 @@ import com.hase.huatuo.healthcheck.model.HealthInfo;
 import com.hase.huatuo.healthcheck.model.healthPK;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonHealthInfoRepository extends JpaRepository<HealthInfo,healthPK> {
+    @Query(value = "select h from HealthInfo h where h.id.staffID = ?1")
+    List<HealthInfo> searchSavedRecordByStaffId(String staffID);
 }
